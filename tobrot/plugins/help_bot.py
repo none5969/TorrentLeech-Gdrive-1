1914a82ee6f99005601b5d42dea5fc01ba9c9b94
alfiananda84 <63289446+alfiananda84@users.noreply.github.com>
2020-06-30 10:43:18 +0700

    Fix derp

--- tobrot/plugins/help_bot.py
+++ tobrot/plugins/help_bot.py
@@ -3,15 +3,17 @@
 # (c) Shrimadhav U K
 
 help_string = f'''
-/help: To get this message
+/mirror: Start mirroring the link to google drive
 
-/mirror [download_url][magnet_link]: Start mirroring the link to google drive
+/mirrorup: Start mirroring the link to Telegram
 
-/mirror unzip | unrar | untar [download_url][magnet_link] : starts mirroring and if downloaded file is any archive , extracts it to google drive
+/tmirror unzip | unrar | untar: Reply to any File on Telegram Start mirroring and if downloaded file is any archive , extracts it to google drive
 
-/mirror archive [download_url][magnet_link]: start mirroring and upload the archived (.tar) version of the download
+/mirror unzip | unrar | untar: starts mirroring and if downloaded file is any archive , extracts it to google drive
 
-/ytdl [youtube-dl supported link]:Reply To message Link To Mirror through youtube-dl 
+/mirror archive: start mirroring and upload the archived (.tar) version of the download
+
+/ytdl: Reply To message Link To Mirror through youtube-dl and Upload to Telegram
 
 /cancel (GID): Reply to the message by which the download was initiated and that download will be cancelled
 
@@ -20,7 +22,6 @@ help_string = f'''
 /stats: Show Stats of the machine the bot is hosted on
 
 /ping: Test Ping 
-
 '''
 
 async def help_bot_message(client, message):
